cfa9e697fed2321f273fdc97101c8a3e4cdba8d7
DamianX2 <damianramz916@gmail.com>
2025-03-29 00:05:19 -0500

    added settings form

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -27,8 +27,8 @@ export function MainNav({
           className={cn(
             "text-sm font-medium transition-colors hover:text-primary",
             route.active
-              ? "text-black dar: text-white"
-              : "teext-muted-foreground"
+              ? "text-black dark: text-white"
+              : "text-muted-foreground"
           )}
         >
           {route.label}
