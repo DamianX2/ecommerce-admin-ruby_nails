f73649a76a1de55e7b65414675d934d3ff9ab92c
DamianX2 <damianramz916@gmail.com>
2025-04-08 00:21:55 -0500

    products

---
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -0,0 +1,25 @@
+import prismadb from "@/lib/prismadb";
+
+import { ProductForm } from "./components/product-form";
+
+const ProductPage = async ({ params }: { params: { productId: string } }) => {
+  const { productId } = await params;
+  const product = await prismadb.product.findUnique({
+    where: {
+      id: productId,
+    },
+    include: {
+      images: true,
+    },
+  });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ProductForm initialData={product} />
+      </div>
+    </div>
+  );
+};
+
+export default ProductPage;
